011d504240f8d279e048e1a30b1c501a61226530
Erik Ernst <eernst@google.com>
2024-05-06 19:33:05 +0200

    WIP

--- lib/type_helper.dart
+++ lib/type_helper.dart
@@ -1,6 +1,6 @@
 class _TypeHelper<X> {
   const _TypeHelper();
-  
+
   bool operator >=(_TypeHelper other) => other is _TypeHelper<X>;
   bool operator <=(_TypeHelper other) => other >= this;
   bool operator >(_TypeHelper other) => this >= other && !(other >= this);
@@ -20,24 +20,22 @@ class _TypeHelper<X> {
 
 extension TypeHelperExtension<X> on X {
   X asA(TypeHelper<X> t) => t.cast(this);
-  bool isA(TypeHelper<Object?> t) => t.containsInstance(this);
-  bool isNotA(TypeHelper<Object?> t) => !t.containsInstance(this);
+  bool isA(TypeHelper<X> t) => t.containsInstance(this);
+  bool isNotA(TypeHelper<X> t) => !t.containsInstance(this);
 }
 
 extension type const TypeHelper<X>._(_TypeHelper<X> _)
     implements _TypeHelper<X> {
-
   // This constructor should be `const`, but we need
   // https://github.com/dart-lang/language/issues/3614
   // before we can do that.
-  TypeHelper(): this._(_TypeHelper<X>());
-  
+  TypeHelper() : this._(_TypeHelper<X>());
+
   R? promoting<R>(Object? toPromote, R Function<Y extends X>(Y) callback) =>
       containsInstance(toPromote) ? _unsafePromote(toPromote, callback) : null;
 
-  R promotingOrElse<R>(Object? toPromote, 
-    R Function<Y extends X>(Y) callback,
-    {required R Function() orElse}) {
+  R promotingOrElse<R>(Object? toPromote, R Function<Y extends X>(Y) callback,
+      {required R Function() orElse}) {
     if (containsInstance(toPromote)) {
       return _unsafePromote(toPromote, callback);
     } else {
@@ -51,17 +49,13 @@ class TypeHelperConstant<X> extends _TypeHelper<X> {
   TypeHelper<X> get asTypeHelper => TypeHelper._(this);
 }
 
-extension TypeHelperConstantExtension on TypeHelperConstant<X> {
+extension TypeHelperConstantExtension<X> on TypeHelperConstant<X> {
   R? promoting<R>(Object? toPromote, R Function<Y extends X>(Y) callback) =>
       containsInstance(toPromote) ? _unsafePromote(toPromote, callback) : null;
 
-  R promotingOrElse<R>(Object? toPromote, 
-    R Function<Y extends X>(Y) callback,
-    {required R Function() orElse}) {
-    if (containsInstance(toPromote)) {
-      return _unsafePromote(toPromote, callback);
-    } else {
-      return orElse();
-    }
-  }
+  R promotingOrElse<R>(Object? toPromote, R Function<Y extends X>(Y) callback,
+          {required R Function() orElse}) =>
+      containsInstance(toPromote)
+          ? _unsafePromote(toPromote, callback)
+          : orElse();
 }
---
+++ test/type_helper_test.dart
@@ -0,0 +1,81 @@
+import 'package:type_helper/type_helper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Relational operators', () {
+    final t1 = TypeHelper<int>();
+    final t2 = TypeHelper<num>();
+
+    expect(t1 <= t2, isTrue);
+    expect(t2 <= t1, isFalse);
+  });
+
+  test('`is` and `as`', () {
+    TypeHelper<num> typeHelper = TypeHelper<int>();
+    /* These are compile-time errors, it's a stupid question:
+       expect(2.isA(typeHelper), isTrue);
+       expect(1.5.isA(typeHelper), isFalse);
+       expect(2.asA(typeHelper), 2);
+       expect(() => 1.5.asA(typeHelper), throws);
+    */
+    expect((2 as num).isA(typeHelper), isTrue);
+    expect((1.5 as num).isA(typeHelper), isFalse);
+    expect((2 as num).asA(typeHelper), 2);
+    expect(() => (1.5 as num).asA(typeHelper), throws);
+  });
+  
+  test('Instance methods doing `is` and `as`', () {
+    TypeHelper<num> typeHelper = TypeHelper<int>();
+
+    expect(typeHelper.containsInstance(2), isTrue);
+    expect(typeHelper.containsInstance(1.5), isFalse);
+    typeHelper.cast(2); // OK.
+    expect(() => typeHelper.cast(1.5), throws);
+  });
+
+  test('The getter `type`', () {
+    TypeHelper<num> typeHelper = TypeHelper<int>();
+    expect(typeHelper.type, int);
+  });
+
+  test('The method `callWith`', () {
+    TypeHelper<num> typeHelper = TypeHelper<int>();
+
+    List<X> createList<X>(TypeHelper<X> typeHelper) =>
+        typeHelper.callWith(<Y>() => <Y>[] as List<X>);
+
+    List<num> xs = createList(typeHelper);
+    expect(xs is List<int>, isTrue);
+  });
+
+  test('The method `promoting`', () {
+    num p = 2;
+    TypeHelper<num> typeHelper = TypeHelper<int>();
+    List<num>? ps = typeHelper.promoting(p, <X extends num>(X x) => <X>[x]);
+    expect(ps.runtimeType, List<int>);
+
+    p = 2.5;
+    ps = typeHelper.promoting(p, <X extends num>(X x) => <X>[x]);
+    expect(ps.runtimeType, Null);
+  });
+
+  test('The method `promotingOrElse`', () {
+    num p = 3;
+    TypeHelper<int> typeHelper = TypeHelper();
+    int c = typeHelper.promotingOrElse(p, <X extends int>(X x) {
+        expect(x.isEven, isFalse);
+        return x as int;
+      },
+      orElse: () => 14,
+    );
+    expect(c, 3);
+
+    p = 3.75;
+    c = typeHelper.promotingOrElse(
+      p,
+      <X extends int>(X x) => throw 'Not reached',
+      orElse: () => 14,
+    );
+    expect(c, 14);
+  });
+}
